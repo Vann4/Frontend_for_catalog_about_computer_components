e33807cbebab8fe97bcadb4984e22c1a857283e8
Vann4 <1vetlan1@mail.ru>
2023-11-19 17:19:28 +0300

    Обработал массив товаров

--- js/main.js
+++ js/main.js
@@ -11,6 +11,14 @@ fetch(url)
         let li_category = document.createElement('li');
         li_category.textContent = array_data.appellation_category
         ul_category.appendChild(li_category)
+        for(let j=0; j<array_data.good.length; j++){
+          array_good = array_data.good[j]
+          console.log(array_good)
+          if (array_good.category_id == 1){
+            console.log('gfg')
+          }
+        }
+        // console.log(array_data)
     }
   }).catch(function(error) {
     console.log('Request failed', error);
