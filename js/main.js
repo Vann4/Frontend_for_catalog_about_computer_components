c9c26f414b454fe4f076da1c1c518978eabd9621
Vann4 <1vetlan1@mail.ru>
2023-11-21 15:04:28 +0300

    Переименовал название переменных, добавил вывод названия товара

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let ul_category = document.querySelector('#category')
-let characteristic_name_processors= document.querySelector('.characteristic_name_processors')
+let characteristics_goods= document.querySelector('.characteristics_goods')
 
 const url = 'http://127.0.0.1:8000/category/'
 
@@ -11,32 +11,46 @@ fetch(url)
         let array_data = data[i]; //Получение данных из массива
         let li_category = document.createElement('li');
         li_category.textContent = array_data.appellation_category
+
+        let id_name = "category" + i;
+        li_category.setAttribute("id", id_name)
         ul_category.appendChild(li_category)
-        let characteristic_name_processors_tag_h2 = document.createElement('h2');
-        characteristic_name_processors_tag_h2.textContent = array_data.appellation_category;
-        characteristic_name_processors_tag_h2.setAttribute("id", "processors")
-        characteristic_name_processors.appendChild(characteristic_name_processors_tag_h2);
+
+        let characteristics_goods_tag_h2 = document.createElement('h2');
+        characteristics_goods_tag_h2.textContent = array_data.appellation_category;
+        characteristics_goods_tag_h2.classList.add('content_categories');
+
+        characteristics_goods.appendChild(characteristics_goods_tag_h2);
 
         for(let j=0; j<array_data.good.length; j++){
           array_good = array_data.good[j]
-          // console.log(array_good)
+          console.log(array_good)
           
+          let product_tag_p = document.createElement('p'); //Для подписи "товар"
+          product_tag_p.textContent = "Товар";
+          characteristics_goods.appendChild(product_tag_p);
+
+          if (product_tag_p.textContent == "Товар"){
+            let characteristics_goods_tag_hr = document.createElement('hr');
+            product_tag_p.before(characteristics_goods_tag_hr);
+          }
+
+          let appellation_goods_tag_p = document.createElement('p'); //Для названий товара
+
+          appellation_goods_tag_p.textContent = array_good.appellation_good;
+          characteristics_goods.appendChild(appellation_goods_tag_p);
+
           for(let jj=0; jj<array_good.characteristic.length; jj++){
             array_characteristic = array_good.characteristic[jj];
-            console.log(array_characteristic)
-            let characteristic_name_processors_tag_p = document.createElement('p');
-            characteristic_name_processors_tag_p.textContent = array_characteristic.characteristic_name;
-            characteristic_name_processors.appendChild(characteristic_name_processors_tag_p);
+            // console.log(array_characteristic)
 
-            if (array_characteristic.characteristic_name == 'Модель'){
-              let characteristic_name_processors_tag_br = document.createElement('hr');
-              console.log(characteristic_name_processors_tag_br);
-              characteristic_name_processors_tag_p.before(characteristic_name_processors_tag_br);
-            }
+            let characteristics_goods_tag_p = document.createElement('p');
+            characteristics_goods_tag_p.textContent = array_characteristic.characteristic_name;
+            characteristics_goods.appendChild(characteristics_goods_tag_p);
 
             let characteristic_processors_tag_p = document.createElement('p');
             characteristic_processors_tag_p.textContent = array_characteristic.characteristic;
-            characteristic_name_processors.appendChild(characteristic_processors_tag_p);
+            characteristics_goods.appendChild(characteristic_processors_tag_p);
           }
         }
         // console.log(array_data)
